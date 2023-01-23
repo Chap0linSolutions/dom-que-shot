7b5e0dddaa8414166fb6df2e4a835ae9848abae0
AmigoDoCarlos <axelpinheiro@gmail.com>
2023-01-23 16:40:05 -0300

    padronizada distância do botão do jogo Eu Nunca

--- src/scenes/EuNunca/Game/index.tsx
+++ src/scenes/EuNunca/Game/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Background from '../../../components/Background';
 import Header from '../../../components/Header';
-import Button from '../../../components/Button';
+import BottomButton from '../../../components/Button/BottomButton';
 import glassIcon from '../../../assets/glass-icon-yellow-background.png';
 import './Game.css';
 
@@ -23,23 +23,23 @@ export default function GamePage({
       <Background>
         <Header logo={coverImg} />
         <div className="EuNuncaDiv">
-          <p className="EuNuncaGameTitle">
-            Crie uma afirmação iniciada com "EU NUNCA..."
-          </p>
-          <div className="EuNuncaSuggestionsDiv">
-            <p className="EuNuncaSuggestionsTitle">
-              Ou, se preferir, use uma de nossas sugestões:
+          <div className="EuNuncaTitleAndSuggestions">
+            <p className="EuNuncaGameTitle">
+              Crie uma afirmação iniciada com "EU NUNCA..."
             </p>
-            {suggestions.map((suggestion) => (
-              <div className="EuNuncaSuggestion" key={suggestion}>
-                <img className="EuNuncaSuggestionIcon" src={glassIcon} />
-                {suggestion}
-              </div>
-            ))}
+            <div className="EuNuncaSuggestionsDiv">
+              <p className="EuNuncaSuggestionsTitle">
+                Ou, se preferir, use uma de nossas sugestões:
+              </p>
+              {suggestions.map((suggestion) => (
+                <div className="EuNuncaSuggestion" key={suggestion}>
+                  <img className="EuNuncaSuggestionIcon" src={glassIcon} />
+                  {suggestion}
+                </div>
+              ))}
+            </div>
           </div>
-          <div className="GameVoteButtonEuNunca">
-            <Button onClick={finishPage}>Continuar</Button>
-          </div>
+          <BottomButton onClick={finishPage}>Continuar</BottomButton>
         </div>
       </Background>
     );
