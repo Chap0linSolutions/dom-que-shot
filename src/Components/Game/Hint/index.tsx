0fc68c117a20e1a0e0456c69e12031b803f4bfd6
AmigoDoCarlos <axelpinheiro@gmail.com>
2023-01-23 11:32:05 -0300

    padronizada distância do botão da tela Hint

--- src/Components/Game/Hint/index.tsx
+++ src/Components/Game/Hint/index.tsx
@@ -1,29 +1,8 @@
 import React from 'react';
 import Background from '../../Background';
 import Header from '../../Header';
-import Button from '../../Button';
 import styled from '@emotion/styled';
-
-const HintPageDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-const HintPageDescription = styled.p`
-  margin: 20px 0;
-  padding: 0 40px;
-  font-weight: 500;
-  font-size: 20px;
-  max-height: 600px;
-  text-align: justify;
-  overflow-y: scroll;
-  overflow-x: hidden;
-  ::-webkit-scrollbar {
-    display: none;
-  }
-`;
+import BottomButton from '../../Button/BottomButton';
 
 interface HintProps {
   coverImg: string;
@@ -45,10 +24,30 @@ export default function HintPage({
       <Header logo={coverImg} goBackArrow={coverPage} title={title} />
       <HintPageDiv>
         <HintPageDescription>{description}</HintPageDescription>
-        <div>
-          <Button onClick={gamePage}>Finalizar</Button>
-        </div>
+        <BottomButton onClick={gamePage}>Finalizar</BottomButton>
       </HintPageDiv>
     </Background>
   );
 }
+
+const HintPageDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+  justify-content: space-between;
+`;
+
+const HintPageDescription = styled.p`
+  margin: 20px 0;
+  padding: 0 40px;
+  font-weight: 500;
+  font-size: 20px;
+  max-height: 600px;
+  text-align: justify;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
