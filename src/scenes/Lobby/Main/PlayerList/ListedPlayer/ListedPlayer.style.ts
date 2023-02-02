aec1ce87728729ee47de8654d2ce3e588a7bec65
AmigoDoCarlos <axelpinheiro@gmail.com>
2023-02-02 18:14:36 -0300

    ajustadas as cervejinhas do lobby

---
+++ src/scenes/Lobby/Main/PlayerList/ListedPlayer/ListedPlayer.style.ts
@@ -0,0 +1,84 @@
+import styled from "@emotion/styled";
+
+export const ListedPlayerDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 36px;
+  background: #ffffff;
+  box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+  padding: 0 10px;
+  margin-bottom: 4px;
+`;
+
+export const AvatarAndNickname = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const AvatarDiv = styled.div`
+    width: 30px;
+    height: 30px;
+  }
+`;
+
+export const Nickname = styled.p`
+  white-space: nowrap;
+  color: rgba(0, 0, 0, 0.54);
+  margin: 0 5px;
+`;
+
+export const BeerGroup = styled.div`
+  display: flex;
+  align-items: center;
+  overflow: scroll;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+
+export const BeerBadge = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0 5px;
+  height: 26px;
+  border-radius: 10px;
+  margin: 0 2px;
+`;
+
+export const FiveBeers = styled(BeerBadge)`
+  background: #664445;
+`;
+
+export const TenBeers = styled(BeerBadge)`
+  background: #777777;
+`;
+
+export const TwentyBeers = styled(BeerBadge)`
+  background: #F9C95C;
+`;
+
+export const BeerBadgeText = styled.p`
+  font-size: 16px;
+  font-weight: 500;
+`;
+
+export const FiveText = styled(BeerBadgeText)`
+  color: rgba(255, 255, 255, 0.54);
+`;
+
+export const TenText = styled(BeerBadgeText)`
+  color: rgba(255, 255, 255, 0.54);
+`;
+
+export const TwentyText = styled(BeerBadgeText)`
+  color: rgba(0, 0, 0, 0.54);
+`;
+
+export const Beer = styled.img`
+  height: 16px;
+  object-fit: scale-down;
+`;
--- src/scenes/Lobby/Main/PlayerList/ListedPlayer/index.tsx
+++ src/scenes/Lobby/Main/PlayerList/ListedPlayer/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Avatar from '../../../../../components/Avatar';
-import Beer from '../../../../../assets/beer.png';
-import './ListedPlayer.css';
+import BeerIcon from '../../../../../assets/beer.png';
+import { FiveText, TenText, TwentyText, AvatarAndNickname, AvatarDiv, Beer, BeerGroup, FiveBeers, ListedPlayerDiv, Nickname, TenBeers, TwentyBeers } from './ListedPlayer.style';
 
 interface ListedPlayerProps {
   seed: string;
@@ -14,22 +14,110 @@ export default function ListedPlayer({
   nickname,
   beers,
 }: ListedPlayerProps) {
+
+  const holdMyBeers = beers;
+  const twentyBeers = Math.floor(holdMyBeers / 20);
+  const tenBeers = Math.floor((holdMyBeers - (20 * twentyBeers)) / 10); 
+  const fiveBeers = Math.floor((holdMyBeers - (20 * twentyBeers) - (10 * tenBeers)) / 5);
+  const singleBeers = holdMyBeers - (20 * twentyBeers) - (10 * tenBeers) - (5 * fiveBeers);
+
   const beerGroup: JSX.Element[] = [];
-  for (let i = 0; i < beers; i++) {
+  for (let i = 0; i < singleBeers; i++) {   //comment
     beerGroup.push(
-      <img key={i} className="ListedPlayerBeer" src={Beer} alt="" />
+      <Beer key={i} src={BeerIcon} alt="" />
+    );
+  }
+  for(let i = 0; i < fiveBeers; i++) {
+    beerGroup.push(
+      <FiveBeers key={500*(i+1)}>
+        <FiveText>5</FiveText>
+        <Beer 
+          key={50*(i+1)} 
+          src={BeerIcon} 
+          alt=""
+        />
+      </FiveBeers>
+    );
+  }
+  for(let i = 0; i < tenBeers; i++) {
+    beerGroup.push(
+      <TenBeers key={100*(i+1)}>
+        <TenText>10</TenText>
+        <Beer 
+          key={10*(i+1)} 
+          src={BeerIcon} 
+          alt=""
+        />
+      </TenBeers>
+    );
+  }
+  for(let i = 0; i < twentyBeers; i++) {
+    beerGroup.push(
+      <TwentyBeers key={200*(i+1)}>
+        <TwentyText>20</TwentyText>
+        <Beer 
+          key={20*(i+1)} 
+          src={BeerIcon} 
+          alt=""
+        />
+      </TwentyBeers>
     );
   }
 
   return (
-    <div className="ListedPlayer">
-      <div className="ListedPlayerAvatarAndNickname">
-        <div className="ListedPlayerAvatar">
-          <Avatar seed={seed} />
-        </div>
-        <p className="ListedPlayerNickname">{nickname}</p>
-      </div>
-      <div className="ListedPlayerBeerGroup">{beerGroup}</div>
-    </div>
+    <ListedPlayerDiv>
+      <AvatarAndNickname>
+        <AvatarDiv>
+          <Avatar seed={seed}/>
+        </AvatarDiv>
+        <Nickname>
+          {nickname}
+        </Nickname>
+      </AvatarAndNickname>
+      
+      <BeerGroup>
+        {beerGroup}
+      </BeerGroup>
+    </ListedPlayerDiv>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // <ListedPlayerDiv>
+    //   <AvatarAndNickname>
+    //     <AvatarDiv>
+    //       <Avatar seed={seed} />
+    //     </AvatarDiv>
+    //     <Nickname>
+    //       {nickname}
+    //     </Nickname>
+    //   </AvatarAndNickname>
+
+    //   <BeerGroup>
+    //     {beerGroup}
+    //   </BeerGroup>
+    // </ListedPlayerDiv>
