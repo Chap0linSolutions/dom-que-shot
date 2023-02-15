d1df8710c1b98911b30ccc9ec4432ff92901a992
AmigoDoCarlos <axelpinheiro@gmail.com>
2023-02-15 12:58:56 -0300

    reformulação do GlobalContextProvider

--- src/contexts/GlobalContextProvider.tsx
+++ src/contexts/GlobalContextProvider.tsx
@@ -1,33 +1,80 @@
-import { createContext, ReactNode, useContext } from 'react';
+import { createContext, ReactNode, useContext, useState } from 'react';
+import { Game } from './games';
 
-interface GlobalContextValue {
-  user_id: string | null;
+export type Player = {
+  avatarSeed: string;
+  nickname: string;
+  beers: number;
+  playerID: number;
+};
+
+export type User = {
+  nickname: string | undefined; 
+  avatarSeed: string | undefined;
+  //useStates removidos (ver comentário nas linhas 26 e 27, mais abaixo)
 }
 
-const GlobalContext = createContext<GlobalContextValue>({ user_id: null });
+export type Room = {
+  code: string | undefined;
+  gameList: Game[];
+  playerList: Player[];
+  currentScreen: string | undefined;
+}
+
+interface GlobalContextValue {
+  user: User,
+  room: Room,
+  // setUser: React.Dispatch<React.SetStateAction<User>>,   //TODO acredito que não é necessário trazer os useStates aqui pra dentro.
+  // setRoom: React.Dispatch<React.SetStateAction<Room>>,   //Verificar se rola de deixar sem mesmo, e em caso afirmativo remover estas duas linhas comentadas
+}
 
 interface GlobalProviderProps {
   children: ReactNode;
 }
 
-export default function GlobalProvider(props: GlobalProviderProps) {
-  const { children } = props;
+//para abastecer os dados iniciais recorremos ao localStorage
+//TODO: verificar a possibilidade de usar cookies no lugar)
+const userData = JSON.parse(window.localStorage.getItem('userData'));   
 
-  const value = {
-    user_id: 'dale',
-  };
+const initialValues: GlobalContextValue = {
+  user: {
+    nickname: undefined,
+    avatarSeed: undefined,
+  },
+  room: {
+    code: undefined,
+    gameList: [],
+    playerList: [],
+    currentScreen: undefined,
+  },
+};
 
-  return (
-    <GlobalContext.Provider value={value}>{children}</GlobalContext.Provider>
-  );
-}
+
+const GlobalContext = createContext<GlobalContextValue>(initialValues);
+const GlobalContextUpdater = createContext<(value: React.SetStateAction<GlobalContextValue>) => void>(undefined); 
+
 
 export function useGlobalContext() {
   const context = useContext(GlobalContext);
+  if (typeof context !== 'undefined'){
+    return context;
+  } throw new Error(`useGlobalContext must be used within a GlobalContext`)
+}
 
-  if (typeof context === 'undefined') {
-    throw new Error(`useGlobalContext must be used within a GlobalContext`);
-  }
+export function useGlobalContextUpdater(){
+  return useContext(GlobalContextUpdater);  
+}
 
-  return context;
+
+export default function GlobalProvider(props: GlobalProviderProps) {
+  const { children } = props;
+  const [globalData, setGlobalData] = useState<GlobalContextValue>(initialValues);
+
+  return (
+    <GlobalContext.Provider value={globalData}> {/* nos permite ler os dados */}              
+      <GlobalContextUpdater.Provider value={setGlobalData}>   {/* nos permite alterar os dados */}
+        {children}
+      </GlobalContextUpdater.Provider>
+    </GlobalContext.Provider>
+  );
 }
